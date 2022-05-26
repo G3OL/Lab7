a95bf528f6aa8fde83c2a30f934dc42ef8128856
Geo_Liapakis <64271221+G3OL@users.noreply.github.com>
2022-05-26 11:55:16 +0300

    Add files via upload

---
+++ rest.example/src/main/java/gr/upatras/rest/example/Application.java
@@ -0,0 +1,14 @@
+package gr.upatras.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("PTP lab ex2 : Rest API commit.");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
